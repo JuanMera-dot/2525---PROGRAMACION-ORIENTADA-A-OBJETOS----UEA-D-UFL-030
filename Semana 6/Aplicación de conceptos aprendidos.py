6b8c420920acf988ed5fa8d26fe128a7b8d45d0a
Juan Mera <jd.merab@uea.edu.ec>
2025-07-02 21:30:33 -0500

    Semana 6

--- Semana 6/Aplicación de conceptos aprendidos.py
+++ Semana 6/Aplicación de conceptos aprendidos.py
@@ -1,5 +1,5 @@
 #Aplicar los conocimientos adquiridos sobre Definición de Clase, Definición de Objeto, Herencia, Encapsulación y Polimorfismo
-# en Python para desarrollar un programa.
+# en Python para desarrollar un programa
 # Definir la clase base
 
 class Telefono:
